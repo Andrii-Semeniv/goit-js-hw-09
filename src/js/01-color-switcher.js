02b4523830a3af322cca7746e36d06077777cee1
Andrii-Semeniv <andrii.semeniv7@gmail.com>
2023-02-12 21:49:00 +0200

    firstTask

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let intervalID = null;
 
+startBtn.addEventListener('click', startEvent);
+stopBtn.addEventListener('click', stopEvent);
+
+stopBtn.setAttribute('disabled', 'true');
+
+function startEvent() {
+  intervalID = setInterval(() => {
+    window.document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+
+  startBtn.setAttribute('disabled', 'true');
+  stopBtn.removeAttribute('disabled');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function stopEvent() {
+  clearInterval(intervalID);
+  startBtn.removeAttribute('disabled');
+  stopBtn.setAttribute('disabled', 'true');
+}
